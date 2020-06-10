cfb93aa6bf3c9188d8aa1d5c5f7131bb678daa49
NTCchengyao <jaster234@lee-cheng.com.tw>
2020-06-10 09:35:39 +0800

    init

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php //>
+
+const APP_DATA = APP_HOME . 'data/';
+const APP_LOG = APP_HOME . 'logs/';
+const APP_NAME = 'mydata';
+
+const DB_NAME = 'pgsql:dbname=mydata_db';
+const DB_USER = 'mydata';
+const DB_PASSWORD = null;
+
+const DEBUG = true;
